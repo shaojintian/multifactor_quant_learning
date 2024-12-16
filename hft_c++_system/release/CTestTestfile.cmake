d0a9e80ad362db511707aba5b37cfe4f99bc18d4
shaojintian <shaojintian99@gmail.com>
2024-12-17 00:12:39 +0800

    repush

---
+++ hft_c++_system/release/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wanting/Downloads/multifactor_quant_learning/hft_c++_system
+# Build directory: /Users/wanting/Downloads/multifactor_quant_learning/hft_c++_system/release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ hft_c++_system/release/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wanting/Downloads/multifactor_quant_learning/hft_c++_system/tests
+# Build directory: /Users/wanting/Downloads/multifactor_quant_learning/hft_c++_system/release/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(engine.unit_tests "/Users/wanting/Downloads/multifactor_quant_learning/hft_c++_system/release/unit_tests")
+set_tests_properties(engine.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/wanting/Downloads/multifactor_quant_learning/hft_c++_system/tests/CMakeLists.txt;35;add_test;/Users/wanting/Downloads/multifactor_quant_learning/hft_c++_system/tests/CMakeLists.txt;0;")
