7b93bcdf51ac4f94604a03f036a272743bfa7e31
shaojintian <shaojintian99@gmail.com>
2024-12-21 19:56:34 +0800

    feature: python hft factor

--- data/binance-bulk-downloader/example/download_aggtrades.py
+++ data/binance-bulk-downloader/example/download_aggtrades.py
@@ -20,3 +20,18 @@ downloader = BinanceBulkDownloader(
     data_type="aggTrades", asset="cm", timeperiod_per_file="monthly"
 )
 downloader.run_download()
+
+
+'''
+[
+  {
+    a: 416690, // 归集成交ID
+    p: "9642.4", // 成交价
+    q: "3", // 成交量
+    f: 595259, // 被归集的首个成交ID
+    l: 595259, // 被归集的末个成交ID
+    T: 1591250548649, // 成交时间
+    m: true, // 是否为主动卖出单
+  }
+]
+'''
--- data/binance-bulk-downloader/example/download_bookdepth.py
+++ data/binance-bulk-downloader/example/download_bookdepth.py
@@ -10,3 +10,28 @@ downloader.run_download()
 # download bookDepth (asset="cm")
 downloader = BinanceBulkDownloader(data_type="bookDepth", asset="cm")
 downloader.run_download()
+
+
+'''
+{
+  "lastUpdateId": 16769853,
+  "symbol": "BTCUSD_PERP", // 交易对
+  "pair": "BTCUSD",		 // 标的交易对
+  "E": 1591250106370,   // 消息时间
+  "T": 1591250106368,   // 撮合时间
+  "bids": [				 // 买单
+    [
+      "9638.0",     	// 价格
+      "431"    			// 数量
+    ]
+  ],
+  "asks": [				// 卖单
+    [
+      "9638.2",			// 价格
+      "12"				// 数量
+    ]
+  ]
+}
+
+
+'''
