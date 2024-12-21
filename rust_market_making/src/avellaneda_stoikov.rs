8e05f96acac266e275db7c00b7f4bd3b00a57e1c
shaojintian <shaojintian99@gmail.com>
2024-12-22 01:03:22 +0800

    feature: python hft factor

--- rust_market_making/src/avellaneda_stoikov.rs
+++ rust_market_making/src/avellaneda_stoikov.rs
@@ -94,10 +94,9 @@ impl AvellanedaStoikov {
 
     // 更新市场状态
     fn update_state(&mut self, price: f64, new_position: f64) {
-        // 记录交易历史
-        
+        let position_delta = new_position - self.state.position;
 
-
+        self.state.cash -= position_delta * price;
         // 更新当前状态
         self.state.position = new_position;
         self.state.current_price = price; // 更新当前价格
